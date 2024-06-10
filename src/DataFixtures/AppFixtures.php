fae455911c55c3b7e4e37665d1d48285e0e5bc06
Mozoou <m.zouheir74@gmail.com>
2024-06-10 22:54:11 +0300

    fixtures added

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Task;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    public function __construct(private readonly UserPasswordHasherInterface $userPasswordHasher)
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $admin = new User();
+        $admin->setEmail('admin@example.com');
+        $admin->setUsername('admin');
+        $hashedPassword = $this->userPasswordHasher->hashPassword(
+            $admin,
+            'admin'
+        );
+        $admin->setPassword($hashedPassword);
+        $admin->setRoles([User::ROLE_ADMIN]);
+
+        $manager->persist($admin);
+        
+        $user = new User();
+        $user->setEmail('user@example.com');
+        $user->setUsername('user');
+        $hashedPassword = $this->userPasswordHasher->hashPassword(
+            $user,
+            'user'
+        );
+        $user->setPassword($hashedPassword);
+
+        $manager->persist($user);
+
+        for ($i = 0; $i < 5; $i++) {
+            $task = new Task();
+            $task->setTitle('Tache'.$i);
+            $task->setContent('content');
+            $task->setAuthor($user);
+            $manager->persist($task);
+        }
+
+        $manager->flush();
+    }
+}
